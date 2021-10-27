3ac2b1980d0b5412e927246154540ffd58d2f1c0
Nekonosuke <nekonosuke2a0@outlook.jp>
2021-10-27 20:47:32 +0900

    累乗の実装

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	int a, b;
+	int a, b, power;
 	char c;
 
 	scanf("%d %c %d", &a, &c, &b);
@@ -21,5 +21,12 @@ int main() {
 		printf("%d\n", a / b);
 	}
 
+	if (c == '^') {
+		power = a;
+		for (int i = 1; i < b; i++) {
+			power *=  a;
+		}
+		printf("%d\n", power);
+	}
 	return 0;
 }
