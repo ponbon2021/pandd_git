4c02e8ff52036bc41ce9fca1b50b3486c6fe9ac7
ponbon <2021daigakunyuusibon@gmail.com>
2021-10-27 20:49:20 +0900

    順列の実装

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	int a, b;
+	int a, b,i, n=1, r=1;
 	char c;
+
 
 	scanf("%d %c %d", &a, &c, &b);
 
@@ -19,6 +20,16 @@ int main() {
 	}
 	if (c == '/') {
 		printf("%d\n", a / b);
+	}
+	if (c == 'p') {
+		for (i = 1; i <= a; i++) {
+			n *= i;
+		}
+		for (i = 1; i <= a - b; i++) {
+			r *= i;
+		}
+		printf("%f\n", (double)n / r);
+		printf("%d\n",)
 	}
 
 	return 0;
